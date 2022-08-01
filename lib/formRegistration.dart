a06b150f8f9540f2fe69865bffc8b52e4d8b78cd
Celxd <celxd250606@gmail.com>
2022-08-01 11:23:56 +0700

    Fix [AGME] Keyboard Overflow

--- lib/formRegistration.dart
+++ lib/formRegistration.dart
@@ -21,91 +21,94 @@ class _formRegistrationState extends State<formRegistration> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text("Form Registration")),
-      body: Container(
-        margin: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            TextField(
-              controller: ctrUsername,
-              obscureText: false,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Username',
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Container(
+          margin: EdgeInsets.all(10),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextField(
+                controller: ctrUsername,
+                obscureText: false,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Username',
+                ),
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            TextField(
-              controller: ctrPassword,
-              obscureText: true,
-              decoration: InputDecoration(
-                border: OutlineInputBorder(),
-                labelText: 'Password',
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Pilih jenis kelamin",
-              style: TextStyle(fontSize: 15),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 1,
-                  groupValue: id,
-                  onChanged: (val) {
+              TextField(
+                controller: ctrPassword,
+                obscureText: true,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                "Pilih jenis kelamin",
+                style: TextStyle(fontSize: 15),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 1,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        id = 1;
+                      });
+                    },
+                  ),
+                  Text("Male"),
+                ],
+              ),
+              Row(
+                children: [
+                  Radio(
+                    value: 2,
+                    groupValue: id,
+                    onChanged: (val) {
+                      setState(() {
+                        id = 2;
+                      });
+                    },
+                  ),
+                  Text("Female")
+                ],
+              ),
+              Text("Phone number: " + phoneNumber),
+              ElevatedButton(
+                  onPressed: () async {
+                    final result = await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const SecondRoute()),
+                    );
+      
                     setState(() {
-                      id = 1;
+                      phoneNumber = result.toString();
                     });
                   },
+                  child: Text("Input Phone number")),
+              Center(
+                  child: Container(
+                width: 200,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  child: Text('Submit'),
                 ),
-                Text("Male"),
-              ],
-            ),
-            Row(
-              children: [
-                Radio(
-                  value: 2,
-                  groupValue: id,
-                  onChanged: (val) {
-                    setState(() {
-                      id = 2;
-                    });
-                  },
-                ),
-                Text("Female")
-              ],
-            ),
-            Text("Phone number: " + phoneNumber),
-            ElevatedButton(
-                onPressed: () async {
-                  final result = await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) => const SecondRoute()),
-                  );
-
-                  setState(() {
-                    phoneNumber = result.toString();
-                  });
-                },
-                child: Text("Input Phone number")),
-            Center(
-                child: Container(
-              width: 200,
-              child: ElevatedButton(
-                onPressed: () {},
-                child: Text('Submit'),
-              ),
-            )),
-          ],
+              )),
+            ],
+          ),
         ),
       ),
     );
